6271a07bc38c0c53f61305442205997d66274297
Falavigna Gustavo <gfalavigna@edesalsl.local>
2020-10-28 17:26:18 -0300

    w/userAccounts

--- client/layouts/topbar.js
+++ client/layouts/topbar.js
@@ -3,13 +3,3 @@ Template.Topbar.events({
 		AccountsTemplates.logout();
 	}
 })
-
-Template.Topbar.helpers({
-	usrwelcome() {
-		if (Meteor.userId() == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-})
