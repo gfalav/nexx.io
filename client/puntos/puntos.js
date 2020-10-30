3173b46ab1835362be8bfdb018131a51c3f75f59
Falavigna Gustavo <gfalavigna@edesalsl.local>
2020-10-30 17:25:35 -0300

    w/map, event and ins puntos

--- client/puntos/puntos.js
+++ client/puntos/puntos.js
@@ -97,3 +97,49 @@ Template.PuntosEdit.events({
 		});
 	}
 });
+
+
+initMap = function(){
+	const uluru = { lat: -33.306673, lng: -66.336460 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+    	zoom: 11,
+    	center: uluru,
+  	});
+
+  	map.addListener("click", (e) => {
+	    placeMarkerAndPanTo(e.latLng, map);
+	});
+};
+
+placeMarkerAndPanTo = function(latLng, map){
+	var data = new Object;
+	var secuencia;
+	if (Puntos.findOne({}, {sort: {secuencia: -1}, limit: 1})){
+		secuencia = Puntos.findOne({}, {sort: {secuencia: -1}, limit: 1}).secuencia + 10;
+	} else {
+		secuencia = 10;
+	};
+
+	data.lat = parseFloat(latLng.lat());
+	data.long = parseFloat(latLng.lng());
+	data.nombre = 'Punto '+ secuencia;
+	data.secuencia = parseFloat(secuencia);
+	data.ang = parseFloat(0);
+	data.dist = parseFloat(0);
+	data.proyectoId = FlowRouter.getParam('proyectoId');
+
+	Meteor.call('puntos.insert', {data}, (err, res)=> {
+		if (err){ 
+			alert(err);
+		} else {
+			FlowRouter.go('/puntos/' + data.proyectoId);
+		}
+	});
+
+
+  new google.maps.Marker({
+    position: latLng,
+    map: map,
+  });
+  map.panTo(latLng);
+}
